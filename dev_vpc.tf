534b508518c2ae5a2ff7dd6ec6683db2388a3803
charanrhel <charan.rhel@gmail.com>
2023-01-17 09:58:12 -0800

    modify the privider.tf file

--- dev_vpc.tf
+++ dev_vpc.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.50.0"
+    }
+  }
+}
+
 data "aws_availability_zones" "available" {
   state = "available"
 }
--- provider.tf
+++ provider.tf
@@ -1,9 +1,8 @@
-terraform {
+/* terraform {
   required_providers {
     aws = {
-      region  = "us-east-2"
       source  = "hashicorp/aws"
       version = "4.50.0"
     }
   }
-}
+} */
